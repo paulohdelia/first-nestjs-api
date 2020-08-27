f5b8beecee2a15e53b8bf591c741549a0b4ae6b2
paulohdelia <paulohdelia@gmail.com>
2020-08-27 02:41:30 -0300

    add crud service

--- src/categories/categories/categories.service.ts
+++ src/categories/categories/categories.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Repository, UpdateResult, DeleteResult } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Categorie } from '../categories.entity';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+    constructor(
+        @InjectRepository(Categorie)
+        private categorieRepository: Repository<Categorie>,
+    ) { }
+
+    async findAll(): Promise<Categorie[]> {
+        return await this.categorieRepository.find();
+    }
+
+    async create(categorie: Categorie): Promise<Categorie> {
+        return await this.categorieRepository.save(categorie);
+    }
+
+    async update(categorie: Categorie): Promise<UpdateResult> {
+        return await this.categorieRepository.update(categorie.id, categorie);
+    }
+
+    async delete(id: number): Promise<DeleteResult> {
+        return await this.categorieRepository.delete(id);
+    }
+}
